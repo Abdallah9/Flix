1f3ea13f278d0b9f511092f85dcb58743a03bdc4
Abdallah Chidjou <raisonousma@gmail.com>
2019-10-25 00:29:32 -0400

    Flix two

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Abdison Salami on 10/22/19.
+//  Copyright © 2019 Abdison Salami. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
